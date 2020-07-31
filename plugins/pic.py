302376508a22b1338de42c8aff29af1246995bea
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-31 17:41:53 +0530

    Add files via upload

--- plugins/pic.py
+++ plugins/pic.py
@@ -1,1 +1,43 @@
+# This is bot coded by Happyboy and used for educational purposes only
+# Copyright of all images uploaded by this bot is goes to respected owners
+
+import os
+from pyrogram import Client,Filters
+from telegraph import upload_file
+
+@Client.on_message(Filters.command(["start"]))
+async def start(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=f"Hello {message.from_user.first_name},\nI'm Telegram To Telegra.ph Image Uploader Bot By HB4All @HB4All_Bot",
+        reply_to_message_id=message.message_id
+    )
+    
+@Client.on_message(Filters.photo)
+async def getimage(client, message):
+    location = "./FILES"
+    if not os.path.isdir(location):
+        os.makedirs(location)
+    imgdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
+    dwn = await client.send_message(
+          text="Downloading...",
+          chat_id = message.chat.id,
+          reply_to_message_id=message.message_id
+          )          
+    await client.download_media(
+            message=message,
+            file_name=imgdir
+        )
+    await dwn.edit_text("Uploading...")
+    try:
+        response = upload_file(imgdir)
+    except Exception as error:
+        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @HB4All1_Bot")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    try:
+        os.remove(imgdir)
+    except:
+        pass
+
 
