d083deab03658bf1d15219515aaf287e5c6ce5ec
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-04 20:06:25 +0530

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -1,6 +1,10 @@
 # This is used for educational purposes only
 # Copyright of all images uploaded by this bot is goes to respected owners
+from typing import Optional, List
 
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
+from telegram.ext import Filters, MessageHandler, run_async
 from telegraph import Telegraph
 import os
 from telethon import events
@@ -44,23 +48,19 @@ async def getimage(client, message, event):
 
 
 @Client.on_message(Filters.text)
-def teleGraph(short_name,page_title,page_contents):
-    # simple function to make a telegraph page 
+def post_telegraph(bot: Bot, update: Update, args: List[str]):
+    short_name = "Done By @Mai_BotS"
     msg = update.effective_message # type: Optional[Message]
-    
-    telegraphy = Telegraph()
-    telegraphy.create_account(short_name=short_name)
+    telegraph = Telegraph()
+    r = telegraph.create_account(short_name=short_name)
+    auth_url = r["auth_url"]
+    LOGGER.info(auth_url)
+    title_of_page = " ".join(args)
     page_content = msg.reply_to_message.text
     page_content = page_content.replace("\n", "<br>")
-    response = telegraphy.create_page(
-            page_title,
-            html_content=page_content
-        )
-
-    return response["url"]
-    # return response
-
-
-print(teleGraph('hh','qqq','sss'))
-
+    response = telegraph.create_page(
+        title_of_page,
+        html_content=page_content
+    )
+    msg.reply_text("https://telegra.ph/{}".format(response["path"]))
 
