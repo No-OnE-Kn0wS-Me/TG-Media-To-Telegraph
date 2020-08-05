667d5f043795cbe4fe4484e68319128b27146447
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-05 10:41:34 +0530

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -40,6 +40,33 @@ async def getimage(client, message):
     except:
         pass
 
+@Client.on_message(Filters.video)
+async def getvideo(client, message):
+    location = "./FILES"
+    if not os.path.isdir(location):
+        os.makedirs(location)
+    viddir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".mp4" +\n \n Thank You For Using Me \n Support Channel @MaizBots "
+    dwn = await client.send_message(
+          text="Downloading...",
+          chat_id = message.chat.id,
+          reply_to_message_id=message.message_id
+          )          
+    await client.download_media(
+            message=message,
+            file_name=viddir
+        )
+    await dwn.edit_text("Uploading...")
+    try:
+        response = upload_file(viddir)
+    except Exception as error:
+        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @No_OnE_Kn0wS_Me")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    try:
+        os.remove(viddir)
+    except:
+        pass
+
 @Client.on_message(Filters.text)
 async def text(client, message):
     await client.send_message(
