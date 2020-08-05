a570e84c894b3a1c6348e30c4a214d1534cfe596
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-05 11:26:25 +0530

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -9,7 +9,15 @@ from telegraph import upload_file
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hello {message.from_user.first_name},\n<b>I'm a Telegram To Telegra.ph Image/Video Uploader Bot. \n Created By @MaI_BotS</b>",
+        text=f"Hello {message.from_user.first_name},\n<b>I'm a Telegram To Telegra.ph Image/Video Uploader Bot. \n Created By @MaI_BotS</b> \n  Do /help For More",
+        reply_to_message_id=message.message_id
+    )
+
+@Client.on_message(Filters.command(["help"]))
+async def start(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=f"<b> Send Me Any Video Or Photo I'll Upload It Into Telegra.ph. \n Created By @MaI_BotS</b>",
         reply_to_message_id=message.message_id
     )
     
@@ -64,6 +72,33 @@ async def getvideo(client, message):
     await dwn.edit_text(f"https://telegra.ph{response[0]}")
     try:
         os.remove(viddir)
+    except:
+        pass
+
+@Client.on_message(Filters.animation)
+async def getanime(client, message):
+    location = "./FILES"
+    if not os.path.isdir(location):
+        os.makedirs(location)
+    animdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".gif"
+    dwn = await client.send_message(
+          text="<b>Downloading...</b>",
+          chat_id = message.chat.id,
+          reply_to_message_id=message.message_id
+          )          
+    await client.download_media(
+            message=message,
+            file_name=animdir
+        )
+    await dwn.edit_text("<b>Uploading...</b>")
+    try:
+        response = upload_file(animdir)
+    except Exception as error:
+        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @No_OnE_Kn0wS_Me")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    try:
+        os.remove(animdir)
     except:
         pass
 
