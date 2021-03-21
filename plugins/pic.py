6f0189507acd7a700520566dffaa86b08d2d14b8
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-21 08:22:22 +0530

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -2,18 +2,32 @@
 # Copyright of all images uploaded by this bot is goes to respected owners
 
 import os
-from pyrogram import Client,Filters
+from pyrogram import Client,filters
 from telegraph import upload_file
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
 
-@Client.on_message(Filters.command(["start"]))
+@Client.on_message(filters.command(["start"]))
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text=f"Hello {message.from_user.first_name},\n<b> I'll Only Work On @MoVieLiNks_oNlY \n You Can't Use Me Here</b>",
-        reply_to_message_id=message.message_id
+        reply_to_message_id=message.message_id,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('Movie Channel', url='https://t.me/BoX_0fFiCe'),
+                    InlineKeyboardButton('Group', url='https://t.me/Movielinks_only')
+                ],
+                [
+                    InlineKeyboardButton('Project Channel', url='https://t.me/Mai_bOTs'),
+                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me')
+                ]
+            ]
+        )
     )
 
-@Client.on_message(Filters.command(["help"]))
+@Client.on_message(filters.command(["help"]))
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
@@ -21,7 +35,7 @@ async def start(client, message):
         reply_to_message_id=message.message_id
     )
 
-@Client.on_message(Filters.text & Filters.group)
+@Client.on_message(filters.text & Filters.group)
 async def text(client, message):
     await client.send_message(
         chat_id=message.chat.id,
