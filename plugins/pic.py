122cd0737e905c116a8e1c20e0f98d5a14743e7d
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-21 17:31:34 +0530

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -5,11 +5,14 @@ import os
 from pyrogram import Client,Filters
 from telegraph import upload_file
 
+
+STICKER_PCK = 'CAACAgUAAxkBAAIFNmBXNYZaoplsDsP_lyh5Lo170CLjAAKVAAMFqVIzXbJ5Od2V8nseBA' 
+
 @Client.on_message(Filters.command(["start"]))
 async def start(client, message):
-    await client.send_message(
+    await client.send_sticker(
         chat_id=message.chat.id,
-        text=f"Hello {message.from_user.first_name},\n<b>I'm a Telegram To Telegra.ph Image/Video Uploader Bot. \n Created By @MaI_BotS</b> \n  Do /help For More",
+        STICKER_PCK, 
         reply_to_message_id=message.message_id
     )
 
